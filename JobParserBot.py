63b63f71b0499153b8bea22fc8693a08d142822f
git@github.com:ArturAyvazyan/appWelox.git <archiforeverything@gmail.com>
2020-08-03 21:26:18 +0300

    BETTER FUNCTIONS

--- JobParserBot.py
+++ JobParserBot.py
@@ -3,7 +3,7 @@ import logging
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
-from yapars import Django, Pyt, Beck, PyJunior, Web, Selo
+from yapars import find_vacancies
 
 
 updater = Updater(token = '1318932508:AAE5l82TDAedYUxSXf0rpAXvzd8d1SwfxOM', use_context = True)
@@ -20,14 +20,14 @@ start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 def django(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text = Django())
+    context.bot.send_message(chat_id=update.effective_chat.id, text = find_vacancies('django Санкт-Петербург'))
     
 
 django_handler = CommandHandler('django', django)
 dispatcher.add_handler(django_handler)
 
 def python(update, context):
-    context.bot.send_message(chat_id = update.effective_chat.id, text = Pyt())
+    context.bot.send_message(chat_id = update.effective_chat.id, text = find_vacancies('python Санкт-Петербург'))
 
 python_handler = CommandHandler('python', python)
 dispatcher.add_handler(python_handler)
@@ -39,19 +39,19 @@ beck_handler = CommandHandler('beck', beck)
 dispatcher.add_handler(beck_handler)
 
 def pyjunior(update, context):
-    context.bot.send_message(chat_id = update.effective_chat.id, text = PyJunior())
+    context.bot.send_message(chat_id = update.effective_chat.id, text = find_vacancies('python junior Санкт-Петербург'))
 
 pyjunior_handler = CommandHandler('pyjunior', pyjunior)
 dispatcher.add_handler(pyjunior_handler)
 
 def web(update, context):
-    context.bot.send_message(chat_id = update.effective_chat.id, text = Web())
+    context.bot.send_message(chat_id = update.effective_chat.id, text = find_vacancies('web developer junior Санкт-Петербург'))
 
 web_handler = CommandHandler('web', web)
 dispatcher.add_handler(web_handler)
 
 def selo(update, context):
-    context.bot.send_message(chat_id = update.effective_chat.id, text = Selo())
+    context.bot.send_message(chat_id = update.effective_chat.id, text = find_vacancies('selenium Санкт-Петербург'))
 
 selo_handler = CommandHandler('selo', selo)
 dispatcher.add_handler(selo_handler)
@@ -63,7 +63,8 @@ def echo(update, context):
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
 
-updater.start_polling()
+updater.start_polling() # - Запуск бота
+updater.idle() # - Вырубаем бота
 
 
 #def caps(update, context):
