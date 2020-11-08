f6878dcae6ebfc1510afb6215a638e5fb8434a9c
Artur <52126850+ArturAyvazyan@users.noreply.github.com>
2020-11-09 01:11:16 +0300

    Update yapars.py

--- yapars.py
+++ yapars.py
@@ -3,18 +3,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+
 driver = webdriver.Chrome(executable_path='C:\webdrivers\chromedriver.exe')
 driver.get('https://rabota.yandex.ru/')
 driver.maximize_window()
 
 
-
-def Beck():
+def page_back():
     driver.back()
     driver.implicitly_wait(5)
     driver.find_element(By.NAME, 'text').clear()
     driver.find_element(By.NAME, 'text').clear()
-
 
 
 def find_vacancies(search_str):
@@ -25,7 +24,6 @@ def find_vacancies(search_str):
     all_wakas = driver.find_elements(By.XPATH, '//a[@class = "link link_upped_yes serp-vacancy__name stat__click"]')
     links = [waka.get_attribute('href') for waka in all_wakas]
  
-
     all_flakas = driver.find_elements(By.XPATH, '//a[@class = "link serp-vacancy__name stat__click"]')
     blinks = [flaka.get_attribute('href') for flaka in all_flakas]
 
@@ -33,11 +31,9 @@ def find_vacancies(search_str):
     boka = [jackass.text for jackass in all_jackass] 
     del boka[1]
 
-
     all_salaries = driver.find_elements(By.CLASS_NAME, 'serp-vacancy__requirements')
     jinglz = [salary.text for salary in all_salaries]
         
-
     all_links = links + blinks
     limit = 5
     min_len = min(len(boka), len(jinglz), len(all_links), limit)
