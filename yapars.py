f7334fb56fccdb71c8bab0079efe25b9646dfad0
Artur <52126850+ArturAyvazyan@users.noreply.github.com>
2020-12-04 23:05:32 +0300

    Update yapars.py

--- yapars.py
+++ yapars.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-driver = webdriver.Chrome(executable_path='C:\webdrivers\chromedriver.exe')
+driver = webdriver.Chrome(executable_path='PATH TO WebDriver')
 driver.get('https://rabota.yandex.ru/')
 driver.maximize_window()
 
